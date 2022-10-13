2bb8a9d2f8599fb222d58a0b35e7d4b2760f15b0
BagioBoi <99057990@mydavinci.nl>
2022-10-13 09:39:52 +0200

    Stap 2

--- Challenges/#SwimmingGarden.py
+++ Challenges/#SwimmingGarden.py
@@ -6,4 +6,13 @@ zwembadDiepte = 1.5
 
 zwembadInhoud = (zwembadLengte * zwembadBreedte * zwembadDiepte)
 zwembadInhoudRound = round(zwembadInhoud, 2)
-print (zwembadInhoudRound)
+print (zwembadInhoudRound, "m3")
+
+gegravenGrond = 25 * zwembadInhoudRound
+afgevoerdGrond = 32.50 * zwembadInhoudRound
+
+print(f'''
+Uitgraven:             €{gegravenGrond}
+Afvoeren grond:        €{afgevoerdGrond}
+Totaal:                €{gegravenGrond + afgevoerdGrond}
+''')
