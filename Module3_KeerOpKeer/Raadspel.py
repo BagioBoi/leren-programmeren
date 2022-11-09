6fc4918d126ba18479f67d586f8e6c4ee09373ec
BagioBoi <99057990@mydavinci.nl>
2022-11-09 10:14:40 +0100

    Raadspel gecheckt

--- Module3_KeerOpKeer/Raadspel.py
+++ Module3_KeerOpKeer/Raadspel.py
@@ -1,50 +1,46 @@
 from random import randint
 score = 0
-totalrounds = 0
-roundsinput = 21 #Dit zorgt ervoor dat het de while loop in gaat voor hoeveel rondes je wilt
+total_rounds = 0
+guess_input = 0
+rounds_input = 21 #Dit zorgt ervoor dat het de while loop in gaat voor hoeveel rondes je wilt
 print('''In dit spel heb je 10 mogelijkheden om een getal tussen van 1 en tot de 1000 te raden.
 Het zal aangeven of het getal hoger of lager is en wanneer je in de buurt bent.''')
-while roundsinput >= 21: 
+while rounds_input >= 21 or rounds_input <= 0: 
     try: #zorgt ervoor dat er geen letters kan worden ingevoerd 
-        roundsinput = int(input("Hoeveel rondes wil je spelen"))
-        if roundsinput <= 20 and roundsinput > 0:
-            break
-        else:
-            print("je kan maximaal 20 rondes spelen")
+        print("je kan maximaal 20 rondes spelen")
+        rounds_input = int(input("Hoeveel rondes wil je spelen"))
     except:
         print("Dat is geen cijfer")
 
-while roundsinput != totalrounds: #Voert de code uit wanneer die twee niet gelijk zijn
-    guessteller = 0
-    secretnumber = randint(1, 1000)
-    while guessteller <= 9: #Het gaat deze loop in en voert het meerdere keren uit totdat er 10 keer is geraden
-        print(secretnumber)
-        guessinput = int(input("Welk getal denk je dat het is?: "))
-        difference = secretnumber - guessinput #Rekent het absolute getal uit
-        if guessinput == secretnumber:
+while total_rounds < rounds_input: #Voert de code uit wanneer die twee niet gelijk zijn
+    guess_teller = 0
+    secret_number = randint(1, 1000)
+    while guess_teller <= 9: #Het gaat deze loop in en voert het meerdere keren uit totdat er 10 keer is geraden
+        try: #ik heb try gebruikt zodat je niet per se een antwoord hoeft te geven en hij helemaal afsluit
+            guess_input = int(input("Welk getal denk je dat het is?: "))
+        except:
+            print("Dat is geen cijfer")
+        difference = secret_number - guess_input #Rekent het absolute getal uit
+        if guess_input == secret_number:
             score += 1
-            roundsinput -= 1
-            print(f"Goed gedaan je hebt het getal {secretnumber} geraden score +1")
+            total_rounds += 1
+            print(f"Goed gedaan je hebt het getal {secret_number} geraden score +1")
             break
-        elif abs (guessinput > secretnumber):
+        elif abs (guess_input > secret_number): #Hier staat de code of je hoger meot raden of lager
             print("Lager")
-        elif abs (guessinput < secretnumber):
+        elif abs (guess_input < secret_number):
             print("Hoger")
         
-        if abs(difference) <= 50 and abs(difference) >= 20:
+        if abs(difference) <= 50 and abs(difference) >= 20: #Hier gebruik ik de variabele difference die heirvoor is uitgerekend om te kijken hoever je van het te raden getal af bent
             print("je bent warm")
         elif abs (difference) <= 20:
             print("Je bent HEEEL heet")
         else:
             print("Niet in de buurt")
 
-        guessteller += 1
-    if roundsinput == totalrounds:
-        print(f"Je totale score was {score}")
-        exit()
-    end = input("Wil je verder").lower()
-    if end in ["nee","no"]:
-        print(f"Je totale score was {score}")
-        exit()
-
+        guess_teller += 1 #dit zit aan het einde van de loop en zorgt ervoor dat je 10x kan raden
+    end = input("Wil je verder").lower() #Hier stelt het de vraag na iedere ronde of je nog een ronde wilt spelen
+    if end in ["nee","no", "n"]:
+        break 
+print(f"Je totale score was {score}")
     
