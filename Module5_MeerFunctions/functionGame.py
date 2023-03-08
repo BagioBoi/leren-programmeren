8c55bc46a02e1520d0b3133787d121199f99e642
BagioBoi <99057990@mydavinci.nl>
2023-03-08 14:03:54 +0100

    game 2e function

--- Module5_MeerFunctions/functionGame.py
+++ Module5_MeerFunctions/functionGame.py
@@ -1,25 +1,43 @@
 #functionGame
+from random import randint
 import random
+nameListMale = ["Noah", "Lucas", "Sem", "Daan", "Levi", "Liam", "James", "Finn", "Luca", "Milan"]
+nameListFemale = ["Julia", "Mila", "Emma", "Nora", "Olivia", "Sophie", "Tess", "Milou", "Zoë", "Yara"]
+points = 0
 
 def namegenerator():
     loop = True
-    nameListMale = ["Noah", "Lucas", "Sem", "Daan", "Levi", "Liam", "James", "Finn", "Luca", "Milan"]
-    nameListFemale = ["Julia", "Mila", "Emma", "Nora", "Olivia", "Sophie", "Tess", "Milou", "Zoë", "Yara"]
-    genderInput = input("Bent u een man of een vrouw?: ")
+    genderInput = input("Bent u een man of een vrouw?: ").lower()
     while loop:
         if genderInput == "man":
             fakeNameInput = input("Wat is je naam?: ")
             name = random.choice(nameListMale)
-            namecheck = input(f"Is dit jouw naam {name}?: ")
+            namecheck = input(f"Is dit jouw naam {name}?: ").lower()
             if namecheck == "ja":
                 loop = False
         elif genderInput == "vrouw":
             fakeNameInput = input("Wat is je naam?: ")
             name = random.choice(nameListFemale)
-            namecheck = input(f"Is dit jouw naam {name}?: ")
+            namecheck = input(f"Is dit jouw naam {name}?: ").lower()
             if namecheck == "ja":
                 loop = False
         else:
             print("Woah die keuze voor gender heb ik helaas niet aangeboden!")
-            genderInput = input("Bent u nou een man of een vrouw?: ")
-    return (name)
+            genderInput = input("Bent u nou een man of een vrouw?: ").lower()
+    return name
+
+def plankSquareM():
+    n1 = randint(1,9)
+    n2 = randint(20,50)
+    answer = n1 * n2
+    try:
+        answercheck = int(input(f"We hebben een plank van {n1} x {n2} nodig voor het hondenhok aan jouw de vraag hoe groot is deze plank?: "))
+    except:
+        print("Dat is geen nummer")
+    if answercheck == answer:
+        print("Goed gedaan je hebt de juiste breedte en lengte van balk gekocht.")
+        global points
+        points += 1
+    else:
+        print("Ohh helaas ik mag hopen dat we hier nog een beetje een normaal hondenhok van kunnen maken.")
+    return points
--- Module5_MeerFunctions/updatedGame.py
+++ Module5_MeerFunctions/updatedGame.py
@@ -1,5 +1,11 @@
 from functionGame import *
-name = ""
-print("Welkom bij mijn spel in dit spel zal je als timmerman of vrouw een avontuur beleven.")
-namegenerator()
-print(f"Nou {name} welkom bij je eerste dag als timmerpersoon")
+gameActive = True
+
+
+while gameActive == True:
+    print("Welkom bij mijn spel in dit spel zal je als timmerman of vrouw een avontuur beleven.")
+    name = namegenerator()
+    print(f"Nou {name} welkom bij je eerste dag als timmerpersoon.\nOns doel is om een prachtig hondenhok te maken.\nDaarvoor zullen we eerst hout nodig hebben.")
+    plankSquareM()
+    ## print (points)/ Hier zal ik de points na het uitvoeren van de function optellen
+    
